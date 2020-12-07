dca679a023154c1e9285575a08a393b23905b82b
Justinas Bedzinskas <bedzinsk@lt.ibm.com>
2020-12-07 15:58:33 +0200

    Turning off inference server model polling

--- includes/run_triton_inference_server.sh
+++ includes/run_triton_inference_server.sh
@@ -3,4 +3,4 @@ docker run --name tis --gpus '"device=0,1,2"' --shm-size=1g --ulimit memlock=-1 
   -p8000:8000 -p8001:8001 -p8002:8002 \
   -v/mnt/azure/models:/models \
   nvcr.io/nvidia/tritonserver:20.09-py3 tritonserver \
-  --model-repository=/models --model-control-mode=poll --repository-poll-secs 5
+  --model-repository=/models
