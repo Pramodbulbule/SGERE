a94e81bc9c76e8ab08d7da3365bd8af7be23f693
Justinas Bedžinskas <bedzinsk@lt.ibm.com>
2021-10-06 09:52:50 +0300

    Cleaning up mount scripts

--- includes/blobfuse_scripts/mount_cameraparams.sh
+++ includes/blobfuse_scripts/mount_cameraparams.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-blobfuse /mnt/azure/cameraparams --tmp-path=/mnt/azure/cameraparams-fusetmp --config-file=/etc/fuse_connections/cameraparams_fuse.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o nonempty  -o allow_other
+blobfuse /mnt/azure/cameraparams --tmp-path=/mnt/azure/fusetmp/cameraparams --config-file=/etc/fuse_connections/cameraparams_fuse.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o nonempty  -o allow_other
