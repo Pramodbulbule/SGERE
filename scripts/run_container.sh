6b9e57882b8ddfe06789dc79028099abb0ac4c98
Justinas Bedzinskas <justinas.bedzinskas.ext@siemensgamesa.com>
2021-12-20 11:21:30 +0000

    Merged PR 20546: Various etcd fixes
    Various etcd fixes

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -4,7 +4,7 @@ base="$(cd "$(dirname "$0")"; pwd)"
 
 docker run --rm -it --net=host \
   --env ANSIBLE_REMOTE_USER=$(cat $base/../includes/user_config/ssh/username) \
-  --env ANSIBLE_ROLES_PATH=/mnt/em-provisioning/roles \
+  --env ANSIBLE_ROLES_PATH=/mnt/em-provisioning/roles:/root/.ansible/roles/ \
   -v "$base/../":/mnt/em-provisioning \
   -v "$base/../includes/user_config/ssh":/mnt/ssh \
   -v "$base/../includes/user_config/home/.bashrc":/root/.bashrc \
