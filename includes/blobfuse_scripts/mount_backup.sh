4858b81501de78f6c105f0749c25d03633c8f784
Pramod Bulbule <pramod.pb.bulbule.ext@siemensgamesa.com>
2022-03-10 12:14:53 +0000

    Merged PR 25071: modified
    new modified

---
+++ includes/blobfuse_scripts/mount_backup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+blobfuse /mnt/azure/backup --tmp-path=/mnt/azure/fusetmp/backup --config-file=/etc/fuse_connections/backup_fuse.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o nonempty -o allow_other
