1f0a3fd2fd9599bffe5d0f4ded69687d0cda71fd
Justinas Bedžinskas <bedzinsk@lt.ibm.com>
2022-03-17 12:13:37 +0200

    log-formatter improvements

--- includes/log-formatter.sh
+++ includes/log-formatter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jq -R -r '. as $line | try (fromjson |  [.asctime, .levelname, .filename, .lineno, .threadName, .message] | .[0] + " " +.[1] + " [" + .[2] + ":" + (.[3] | tostring) + "] " + .[4] + ": " +.[5]) catch $line'
+jq -R -r '. as $line | try (fromjson |  [.asctime, .levelname, .filename, .lineno, .threadName, .message, .exc_info] | .[0] + " " +.[1] + " [" + .[2] + ":" + (.[3] | tostring) + "] " + .[4] + ": " +.[5] + " " + .[6]) catch $line'
